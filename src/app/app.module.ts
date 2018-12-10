f65c3ffa1cbf1350d66afa652dde74d90a8b1095
studentair <43929576+studentair@users.noreply.github.com>
2018-12-10 18:23:36 +0100

    changes
    Dawid Martyka

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MapsComponent } from './components/maps/maps.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BlogComponent } from './components/blog/blog.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'maps',
+    component: MapsComponent,
+  },
+  {
+    path: 'quiz',
+    component: QuizComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+  }];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MapsComponent,
+    ContactComponent,
+    QuizComponent,
+    NavBarComponent,
+    BlogComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
